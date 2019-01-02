5df3365863f05f023f892f6c3a4c899337d74db1
Sven Werlen <sven.werlen@desjardins.com>
2019-01-02 10:57:44 -0500

    Scraping Spells

--- scraping/extract-spells.py
+++ scraping/extract-spells.py
@@ -3,6 +3,7 @@
 
 import urllib
 import yaml
+import sys
 from lxml import html
 try:
     from BeautifulSoup import BeautifulSoup
@@ -10,38 +11,59 @@ except ImportError:
     from bs4 import BeautifulSoup
     from bs4 import NavigableString
 
-DEBUG = False
+## Configurations pour le lancement
+MOCK_LIST = "mocks/sortsListeA.html" # décommenter pour tester avec une liste pré-téléchargée
+MOCK_SORT = "mocks/sort1.html"       # décommenter pour tester avec un sort pré-téléchargé
 
-liste = []
-
-PROPERTIES = [ u"Nom", u"École", u"Niveau", u"Portée", u"Cibles", u"Zone", u"Zone d'effet", u"Effet", u"Temps d’incantation", u"Temps d'incantation", u"Composantes", u"Cible", u"Durée", u"Jet de sauvegarde", u"Résistance à la magie", u"Description"]
-
-
-# récupérer la liste des pages
-url = ["http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Liste%20des%20sorts.ashx",
+URLs = ["http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Liste%20des%20sorts.ashx",
        "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Liste%20des%20sorts%20(suite).ashx",
        "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Liste%20des%20sorts%20(fin).ashx"]
 
+PROPERTIES = [ u"Nom", u"École", u"Niveau", u"Portée", u"Cibles", u"Zone", u"Zone d'effet", u"Effet", u"Temps d’incantation", u"Temps d'incantation", u"Composantes", u"Cible", u"Durée", u"Jet de sauvegarde", u"Résistance à la magie", u"Description"]
+
+# vérification des paramètres
+if len(sys.argv) < 2:
+    print "Usage: %s [HTML|TEXT]" % sys.argv[0]
+    print " - HTML: contenu extrait en tant que HTML"
+    print " - TEXT: contenu extrait en tant que TEXT"
+    exit(1)
+
+# message d'accueil
+html = sys.argv[1].lower() == "html"
+if html:
+    print "Génération du fichier YAML pour les sorts en mode HTML"
+else:
+    print "Génération du fichier YAML pour les sorts en mode TEXT"
+
+
+
+liste = []
+
+
 list = []
-if DEBUG:
-    parsed_html = BeautifulSoup(open("sortsA.txt"),features="lxml")
+if MOCK_LIST:
+    parsed_html = BeautifulSoup(open(MOCK_LIST),features="lxml")
     list = parsed_html.body.find(id='PageContentDiv').find_all('li')
 else:
     list = []
-    for u in url:
+    for u in URLs:
         parsed_html = BeautifulSoup(urllib.urlopen(u).read(),features="lxml")
         list += parsed_html.body.find(id='PageContentDiv').find_all('li')
 
-#for l in list:
-#    element = l.find_next('a')
-#    title = element.get('title')
-#    link  = element.get('href')
-#    print "Processing %s (%s)" % (title,link)
-#exit(1)
-
+#
+# cette fonction se charge d'extraire le texte de la partie HTML
+# en explorant certaines balises. Malheureusement, le format des
+# pages peut différer d'une fois à l'autre.
+#
 def extractText(list):
     text = ""
+    
     for el in list:
+        
+        if html:
+            text += repr(el)
+            continue
+        
         if el.name == 'br':
             if text[-2:] != '\n\n':
                 text += '\n'
@@ -79,11 +101,11 @@ for l in list:
     sort['Nom']=title
     sort['Reference']=pageURL
     
-    if DEBUG:
-        content = BeautifulSoup(open("sort5.html"),features="lxml").body.find(id='PageContentDiv')
+    if MOCK_SORT:
+        content = BeautifulSoup(open(MOCK_SORT),features="lxml").body.find(id='PageContentDiv')
     else:
         content = BeautifulSoup(urllib.urlopen(pageURL).read(),features="lxml").body.find(id='PageContentDiv')
-              
+    
     # lire les attributs
     text = ""
     for attr in content.find_all('b'):
@@ -111,10 +133,10 @@ for l in list:
     # ajouter sort
     liste.append(sort)
     
-    if DEBUG:
+    if MOCK_SORT:
         break
 
-if DEBUG:
+if MOCK_SORT:
     print yaml.safe_dump(liste,default_flow_style=False, allow_unicode=True)
     exit(1)
 
