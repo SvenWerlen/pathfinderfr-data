20fb20b245b180a2fa5152ebee11d0f4f73ece59
Sven Werlen <sven.werlen@desjardins.com>
2019-02-10 18:29:24 -0500

    Fix issue with L'

--- scraping/extract-classes.py
+++ scraping/extract-classes.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
 import urllib.request
@@ -13,8 +13,22 @@ from lxml import html
 MOCK_CLASS = None
 #MOCK_CLASS = "mocks/classe-alchimiste.html"       # décommenter pour tester avec une classe pré-téléchargée
 #MOCK_CLASS = "mocks/classe-antipaladin.html"       # décommenter pour tester avec une classe pré-téléchargée
+#MOCK_CLASS = "mocks/classe-inquisiteur.html"       # décommenter pour tester avec une classe pré-téléchargée
+#MOCK_CLASS = "mocks/classe-arpenteur.html"       # décommenter pour tester avec une classe pré-téléchargée
 
 URLs = [
+    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Barbare.ashx", 'source': 'MJ'},
+    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Barde.ashx", 'source': 'MJ'},
+    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Druide.ashx", 'source': 'MJ'},
+    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Ensorceleur.ashx", 'source': 'MJ'},
+    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Guerrier.ashx", 'source': 'MJ'},
+    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Magicien.ashx", 'source': 'MJ'},
+    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Moine.ashx", 'source': 'MJ'},
+    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Paladin.ashx", 'source': 'MJ'},
+    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Pr%c3%aatre.ashx", 'source': 'MJ'},
+    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.R%c3%b4deur.ashx", 'source': 'MJ'},
+    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Roublard.ashx", 'source': 'MJ'},
+
     {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Alchimiste.ashx", 'source': 'MJRA'},
     {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Antipaladin.ashx", 'source': 'MJRA'},
     {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Chevalier.ashx", 'source': 'MJRA'},
@@ -22,24 +36,52 @@ URLs = [
     {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Inquisiteur.ashx", 'source': 'MJRA'},
     {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Oracle.ashx", 'source': 'MJRA'},
     {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Sorci%c3%a8re.ashx", 'source': 'MJRA'},
-    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Magus.ashx", 'source': 'MJRA'},
-    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Ninja.ashx", 'source': 'MJRA'},
-    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Pistolier.ashx", 'source': 'MJRA'},
-    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Samoura%c3%af.ashx", 'source': 'MJRA'},
+    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Magus.ashx", 'source': 'AM'},
+    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Ninja.ashx", 'source': 'AG'},
+    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Pistolier.ashx", 'source': 'AG'},
+    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Samoura%c3%af.ashx", 'source': 'AG'},
     
-    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Arcaniste.ashx", 'source': 'MJRA'},
-    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Bretteur.ashx", 'source': 'MJRA'},
-    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Chaman.ashx", 'source': 'MJRA'},
-    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Chasseur.ashx", 'source': 'MJRA'},
-    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Enqu%c3%aateur.ashx", 'source': 'MJRA'},
-    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Lutteur.ashx", 'source': 'MJRA'},
-    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Pr%c3%aatre%20combattant.ashx", 'source': 'MJRA'},
-    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Sanguin.ashx", 'source': 'MJRA'},
-    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Scalde.ashx", 'source': 'MJRA'},
-    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Tueur.ashx", 'source': 'MJRA'},    
+    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Arcaniste.ashx", 'source': 'MCA'},
+    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Bretteur.ashx", 'source': 'MCA'},
+    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Chaman.ashx", 'source': 'MCA'},
+    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Chasseur.ashx", 'source': 'MCA'},
+    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Enqu%c3%aateur.ashx", 'source': 'MCA'},
+    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Lutteur.ashx", 'source': 'MCA'},
+    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Pr%c3%aatre%20combattant.ashx", 'source': 'MCA'},
+    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Sanguin.ashx", 'source': 'MCA'},
+    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Scalde.ashx", 'source': 'MCA'},
+    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Tueur.ashx", 'source': 'MCA'},    
+    
+    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Cin%c3%a9tiste.ashx", 'source': 'AO'},    
+    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Hypnotiseur.ashx", 'source': 'AO'},    
+    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.M%c3%a9dium.ashx", 'source': 'AO'},    
+    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Occultiste.ashx", 'source': 'AO'},    
+    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Psychiste.ashx", 'source': 'AO'},    
+    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Spirite.ashx", 'source': 'AO'},
+    
+    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Archer-mage.ashx", 'source': 'MJ', 'prestige': True},
+    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Arpenteur%20dhorizon.ashx", 'source': 'MJRA', 'prestige': True},
+    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Assassin.ashx", 'source': 'MJ', 'prestige': True},
+    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Champion%20occultiste.ashx", 'source': 'MJ', 'prestige': True},
+    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Chroniqueur.ashx", 'source': 'MJ', 'prestige': True},
+    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Disciple%20draconien.ashx", 'source': 'MJ', 'prestige': True},
+    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Duelliste.ashx", 'source': 'MJ', 'prestige': True},
+    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Fid%c3%a8le%20d%c3%a9fenseur.ashx", 'source': 'MJRA', 'prestige': True},
+    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Gardien%20de%20la%20nature.ashx", 'source': 'MJRA', 'prestige': True},
+    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Gardien%20du%20savoir.ashx", 'source': 'MJ', 'prestige': True},
+    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.H%c3%a9raut.ashx", 'source': 'MJRA', 'prestige': True},
+    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Ma%c3%aetre%20chYmiste.ashx", 'source': 'MJRA', 'prestige': True},
+    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Ma%c3%aetre%20des%20ombres.ashx", 'source': 'MJ', 'prestige': True},
+    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Ma%c3%aetre%20espion.ashx", 'source': 'MJRA', 'prestige': True},
+    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Mystificateur%20profane.ashx", 'source': 'MJ', 'prestige': True},
+    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Proph%c3%a8te%20enrag%c3%a9.ashx", 'source': 'MJRA', 'prestige': True},
+    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Th%c3%a9urge%20mystique.ashx", 'source': 'MJ', 'prestige': True},
+    {'link': "http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Vengeur%20sacr%c3%a9.ashx", 'source': 'MJRA', 'prestige': True},
+    
 ]
 
-PROPERTIES = [ u"Caractéristique associée", u"caractéristique associée", u"Formation nécessaire", u"Formation nécesssaire", u"Malus d’armure"]
+CONN = (u'Connaissances (exploration souterraine)',u'Connaissances (folklore local)',u'Connaissances (géographie)',u'Connaissances (histoire)',
+         'Connaissances (ingénierie)',u'Connaissances (mystères)',u'Connaissances (nature)',u'Connaissances (noblesse)',u'Connaissances (plans)',u'Connaissances (religion)');
 
 # vérification des paramètres
 if len(sys.argv) < 1:
@@ -51,8 +93,10 @@ if len(sys.argv) < 1:
 # cette fonction convertit le nom 'Le barbare' => 'Barbare'
 #
 def cleanName(name):
-    if name.lower().startswith(u"le ") or name.lower().startswith(u"l' "):
+    if name.lower().startswith(u"le "):
         name = name[3:]
+    elif name.lower().startswith(u"l'"):
+        name = name[2:]
     return name[:1].upper() + name[1:].lower()
 
 #
@@ -70,7 +114,15 @@ def findAfter(html, afterTag, afterCond, searched):
 def findProperty(html, propName):
     for el in html:
         if el.name == 'b' and el.text.lower().startswith(propName.lower()):
-            return el.next_sibling.replace('.','').strip()
+            value = ""
+            for e in el.next_siblings:
+                if e.name == 'br':
+                    break
+                elif e.string:
+                    value += e.string
+                else:
+                    value += e
+            return value.replace('.','').strip()
     return None
 
 #
@@ -105,6 +157,10 @@ for data in URLs:
     # référence
     cl[u'Référence'] = link
     
+    # prestige
+    if 'prestige' in data.keys() and data['prestige']:
+        cl[u'Prestige'] = True
+    
     # source
     cl[u'Source'] = data['source']
 
@@ -125,15 +181,29 @@ for data in URLs:
     # compétences de classe
     cl[u'CompétencesDeClasse'] = []
     section = jumpTo(html, 'h2',{'class':'separator'}, u"Compétences de classe")
+    if section is None:
+        section = jumpTo(html, 'h2',{'class':'separator'}, u"Compétences de la classe")
+    
     for s in section:
         if s.name == 'a' and len(s.text) > 3:
             value = s.text
             if value == u"Connaissances":
                 idx = s.next_sibling.index(')')
                 if idx > 0:
-                    value += s.next_sibling[0:idx+1]
+                    value += s.next_sibling[0:idx+1].lower()
             
-            cl[u'CompétencesDeClasse'].append({u'Compétence':value.strip()})
+            value = value.replace('’',"'")
+            # hacks
+            if value == u'Utilisation des objets magiques':
+                value = u'Utilisation d\'objets magiques'
+            elif value == u'Connaissances (mystère)':
+                value = u'Connaissances (mystères)'
+        
+            if value == u'Connaissances (toutes)' or value == u'Connaissances (tous les domaines)':
+                for c in CONN:
+                    cl[u'CompétencesDeClasse'].append({u'Compétence':c})
+            else:
+                cl[u'CompétencesDeClasse'].append({u'Compétence':value.strip()})
         elif s.name == 'br':
             break;
 
