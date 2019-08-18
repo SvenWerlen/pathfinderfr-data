050afb8426c4164a91be0270a849500fbf60dbf4
Sven Werlen <sven.werlen@desjardins.com>
2019-08-17 23:46:44 -0400

    Mise à jour des sceptres magiques

--- scraping/extract-magic-scepters.py
+++ scraping/extract-magic-scepters.py
@@ -9,7 +9,7 @@ import re
 from bs4 import BeautifulSoup
 from lxml import html
 
-from libhtml import table2text, extractBD_Type1, extractBD_Type2, html2text, cleanName
+from libhtml import table2text, extractBD_Type1, extractBD_Type2, html2text, cleanName, mergeYAML
 
 ## Configurations pour le lancement
 MOCK_MAGIC = None
@@ -20,6 +20,10 @@ MOCK_MAGIC_ITEM = None
 #MOCK_MAGIC_ITEM = "mocks/magic-carreau.html"      # décommenter pour tester avec détails pré-téléchargé
 
 TEXTE = 'Si l’arme bénéficie seulement d’un bonus d’altération, son niveau de lanceur de sorts est égal à trois fois son bonus. Si l’objet possède un bonus d’altération ainsi que des propriétés magiques, le plus haut niveau de lanceur de sorts entre les deux est celui qui doit être considéré. Armes à distance et projectiles. \n\nLe bonus d’altération d’une arme à projectiles ne se cumule pas avec le bonus d’altération de ses munitions. Seule la plus haute valeur est prise en compte.'
+
+FIELDS = ['Nom', 'Type', 'Prix', 'Source', 'Emplacement', 'Poids', 'Aura', 'NLS', 'Conditions', 'Coût', 'Description', 'Référence' ]
+MATCH = ['Nom']
+
 
 liste = []
 
@@ -63,6 +67,8 @@ propList = []
 
 tables = content.find_all('table',{'class':['tablo col1centre']})
 
+print("Extraction des sceptres magiques...")
+
 liste = []
 exists = []
 
@@ -105,7 +111,7 @@ for t in tables:
             data = { **data, **TABLEDEF[tableIdx][3] }
         
         # débogage
-        #print("Traitement de %s..." % nom.strip())
+        print("Traitement de %s..." % nom.strip())
         
         
         # get description from metamagie
@@ -140,52 +146,40 @@ for t in tables:
                 exit(1)
         
         element = {}
-        element["01Nom"] = data["nomAlt"]
-        element["02Type"] = TYPE
-        element["03Prix"] = data["prixAlt"]
-        element["04Source"] = "MJ"
-        element["20Description"] = data["descr"]
-        element["21Référence"] = reference
+        element["Nom"] = data["nomAlt"]
+        element["Type"] = TYPE
+        element["Prix"] = data["prixAlt"]
+        element["Source"] = "MJ"
+        element["Description"] = data["descr"]
+        element["Référence"] = reference
         
         # infos additionnelles
         if "emplacement" in data:
-            element["05Emplacement"] = data["emplacement"]
+            element["Emplacement"] = data["emplacement"]
         if "poids" in data:
-            element["06Poids"] = data["poids"]
+            element["Poids"] = data["poids"]
         if "aura" in data:
-            element["07Aura"] = data["aura"]
+            element["Aura"] = data["aura"]
         if "nls" in data:
             # NLS parfois variable
             if isinstance(data["nls"], int):
-                element["08NLS"] = data["nls"]
+                element["NLS"] = data["nls"]
             else:
-                element["20Description"] = "NLS: " + data["nls"] + "\n\n" + element["20Description"]
+                element["Description"] = "NLS: " + data["nls"] + "\n\n" + element["Description"]
         if "conditions" in data:
-            element["09Conditions"] = data["conditions"]
+            element["Conditions"] = data["conditions"]
         if "coût" in data:
-            element["10Coût"] = data["coût"]
-        
-        element["EMPTY"] = ""
-        
+            element["Coût"] = data["coût"]
+                
         if not data["nomAlt"] in exists:
             exists.append(data["nomAlt"])
             liste.append(element)
     
 #exit(1)
 
-yml = yaml.safe_dump(liste,default_flow_style=False, allow_unicode=True)
-yml = yml.replace('01Nom','Nom')
-yml = yml.replace('02Type','Type')
-yml = yml.replace('03Prix','Prix')
-yml = yml.replace('04Source','Source')
-yml = yml.replace('05Emplacement','Emplacement')
-yml = yml.replace('06Poids','Poids')
-yml = yml.replace('07Aura','Aura')
-yml = yml.replace('08NLS','NLS')
-yml = yml.replace('09Conditions','Conditions')
-yml = yml.replace('10Coût','Coût')
-yml = yml.replace('20Description','Description')
-yml = yml.replace('21Référence','Référence')
-yml = yml.replace("EMPTY: ''",'')
-print(yml)
 
+print("Fusion avec fichier YAML existant...")
+
+HEADER = ""
+
+mergeYAML("../data/magic.yml", MATCH, FIELDS, HEADER, liste)
