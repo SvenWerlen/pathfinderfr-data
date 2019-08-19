b21f81eabc482bf6ec2599d1ba1b5ad7f77fa859
Sven Werlen <sven.werlen@desjardins.com>
2019-08-18 20:35:23 -0400

    Améliorer description des conditions

--- scraping/extract-conditions.py
+++ scraping/extract-conditions.py
@@ -9,7 +9,7 @@ import re
 from bs4 import BeautifulSoup
 from lxml import html
 
-from libhtml import jumpTo, mergeYAML
+from libhtml import jumpTo, html2text, mergeYAML
 
 
 ## Configurations pour le lancement
@@ -53,45 +53,16 @@ for s in section:
             condition['Description'] = descr.strip()
             liste.append(condition)
             condition = {'Source':SOURCE}
-            brCount = 0
         descr = ""
         condition['Nom'] = s.text.replace('¶','').strip()
         if advantage:
             condition['Nom'] += " (avantage)"
         newObj = True
         
-        for e in s.children:
-            if e.name == 'a':
-                condition['Référence']=URL + e['href']
-    elif s.name == 'br':
-        descr += '\n'
-    elif s.name is None or s.name == 'a' or s.name == 'i' or s.name == 'b':
-        if s.string is None:
-            for s2 in s.children:
-                if s2.name is None or s2.name == 'a' or s2.name == 'b' or s2.name == 'i':
-                    descr += s2.string
-        else:
-            descr += s.string
-    elif s.name == 'div':
-        for s2 in s.children:
-            if s2.name is None or s2.name == 'a' or s2.name == 'b' or s2.name == 'i':
-                if not s2.string is None:
-                    descr += s2.string
-    elif s.name == 'ul':
-        for s2 in s.children:
-            if s2.name == 'li':
-                descr += "- " + s2.text + "\n"
-    elif s.name == 'center':
-        for s2 in s.children:
-            if s2.name == 'table':
-                for s3 in s2.children:
-                    if s3.name == 'tr':
-                        descr += "\n- "
-                        for s4 in s3.children:
-                            if s4.name == 'td':
-                                descr += s4.text + " "
-        descr += "\n\n"
-
+        condition['Référence']= URL + s.find('a')['href']
+                
+    else:
+        descr += html2text(s)
 
 ## last element
 condition['Description'] = descr.replace('\n','').strip()
