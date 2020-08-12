dcc09f236d5879577401bdf3d8104abcb5209c45
Sven Werlen <sven.werlen@pm.me>
2020-08-11 22:30:59 -0400

    Traits improved

---
+++ foundryvtt/convertTraits.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import yaml
+import json
+import typing
+import sys
+import csv
+
+from libData import *
+
+data = None
+with open("../data/traits.yml", 'r') as stream:
+    try:
+        data = yaml.safe_load(stream)
+    except yaml.YAMLError as exc:
+        print(exc)
+
+list = []
+
+duplicates = []
+for d in data:
+  key = d['Race'] + d['Nom'] if 'Race' in d else d['Nom']
+  if key in duplicates:
+    print("Ignoring duplicate: " + d['Nom'])
+    continue
+  duplicates.append(key)
+
+  subtype = None
+  name = cleanTitle(d['Nom'])
+  m = re.search('Trait (de )?(.+): (.*)', name)
+  if m:
+    subtype = m.group(2).split(' ')[0]
+    subtype = subtype[0].upper() + subtype[1:]
+    name = m.group(3)
+  
+  replace = ""
+  if 'Remplace' in d:
+    for r in d['Remplace']:
+      replace += r + ", "
+    replace = replace[:-2]
+      
+  description = d['Description'].strip()
+  if description.startswith("."):
+    description = description[1:].strip()
+  
+  if "Race" in d:
+    description = "<p><i>%s</i></p><p><b>Race :</b> ##racesfr|%s##<br/><b>Remplace :</b> %s<p/><p><b>Référence :</b> <a href=\"%s\" parent=\"_blank\">pathfinder-fr.org</a></p>" \
+      % (description, d['Race'], replace if 'Remplace' in d else "-", d['Référence'])
+  else:
+    description = "<p><i>%s</i></p><p><b>Référence :</b> <a href=\"%s\" parent=\"_blank\">pathfinder-fr.org</a></p>" \
+      % (description, d['Référence'])
+  description = improveDescription(description, name)
+
+  el = {
+    "name": name,
+    "type": "feat",
+    "data": {
+      "description": {
+        "value": description,
+      },
+      "featType": "racial" if "Race" in d or subtype == "Race" else "trait",
+      "tags": [[d['Race']]] if "Race" in d else [],
+    },
+    "flags": {},
+    "img": "icons/svg/mystery-man.svg"
+  }
+  if subtype and subtype != "Race":
+    el["data"]["tags"].append([subtype])
+      
+  list.append(el)
+
+
+list = mergeWithLetContribute(list, "letscontribute/traitsfr.json", False)
+
+# écrire le résultat dans le fichier d'origine
+outFile = open("data/traits.json", "w")
+outFile.write(json.dumps(list, indent=3))
